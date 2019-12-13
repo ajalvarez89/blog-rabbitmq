3cb67b863f7b2b64ebf9325e10cbb72ddcfef211
Alvaro Alvarez <alvaro.alvarez@youse.com.co>
2019-12-13 11:24:52 -0500

    adding enum into post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,8 @@
 class Post < ApplicationRecord
+
+  enum status: [:created, :canceled, :payed, :completed]
+
+  def change
+    add_column :posts, :status, :integer, default: 0
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_30_191447) do
+ActiveRecord::Schema.define(version: 2019_12_13_162248) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "status", default: 0
   end
 
 end
